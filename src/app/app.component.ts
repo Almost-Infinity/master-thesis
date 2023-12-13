4e32632de018155d06c99c0a2418d8e6e1d9efc6
Almost-Infinity <its.alfinity@gmail.com>
2023-12-13 11:54:13 +0200

    feat: add header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [ NxWelcomeComponent, RouterModule, MatToolbarModule, MatButtonModule, MatIconModule ],
   selector: 'master-thesis-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
